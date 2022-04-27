f6af86f2c87b27a23961446521c57d4cbb3ddcc6
korenev6052 <korenev6052@gmail.com>
2022-04-27 11:02:44 +0300

    babel was added; rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'production'
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
